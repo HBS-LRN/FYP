47ae628d40b38e2220d64ab5b04d115a7ece619a
ChunYapBeep <127012228+ChunYapBeep@users.noreply.github.com>
2023-05-04 02:51:18 +0800

    cy3ampush

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -3,7 +3,11 @@
 <head>
     <body>
     @foreach ($logs as $log)
-<p>{{$log->}}</p>
+<p>{{$log->user_id}}</p>
+<p>{{$log->table_name}}</p>
+<p>{{$log->row_id}}</p>
+<p>{{$log->json_decode(new_data['meal_price'])}}</p>
+<p>{{$log->created_at}}</p>
 @endforeach
 </body>
 </head>
